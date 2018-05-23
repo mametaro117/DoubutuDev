90ae53929f35a8fa9eeb2e44c983324fb9a352ce
mametaro117 <ytfeek1@yahoo.co.jp>
2018-05-24 03:14:27 +0900

    コストバーの実装、ボタンのイベント追加のテスト
    コストを増やすスクリプトの追加とデリゲートのテスト

---
+++ DoubutuDev/Assets/01.Scripts/Common/AnimalButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalButton : MonoBehaviour {
+
+    private Button btn;
+
+	// Use this for initialization
+	void Start () {
+        btn = GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)) Test();
+	}
+
+    void Test()
+    {
+        btn.onClick.AddListener(TapAnimalButton);
+    }
+
+    void TapAnimalButton()
+    {
+        Debug.Log("Tap");
+    }
+}
---
+++ DoubutuDev/Assets/01.Scripts/Common/CostScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostScript : MonoBehaviour {
+
+    //  コストの最大値
+    private const float MaxCost = 500;
+    [SerializeField]
+    private float DefaultCost = 100;
+    [SerializeField]
+    private float CostPoint;
+
+    private RectTransform rect;
+
+
+	void Start () {
+        //  コストの初期化
+        CostPoint = DefaultCost;
+        //  コンポーネントの参照
+        rect = GetComponent<RectTransform>();
+	}
+	
+	void Update () {
+        if(CostPoint < MaxCost) CostPoint += Time.deltaTime * 25;
+        CostFixed();
+    }
+
+
+
+    //  値をコストバーに反映
+    public void CostFixed()
+    {
+        rect.sizeDelta = new Vector2(CostPoint / MaxCost * 500, rect.sizeDelta.y);
+    }
+}
