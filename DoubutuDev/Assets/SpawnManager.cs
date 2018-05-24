ca279453b571f8222c97040fd5fbdfdacb8fb823
mametaro117 <ytfeek1@yahoo.co.jp>
2018-05-24 15:38:35 +0900

    クリックした場所に動物を配置するスクリプトの作成

---
+++ DoubutuDev/Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            GameObject obj = Instantiate(prefab, new Vector3 (pos.x, pos.y, pos.z), prefab.transform.rotation);
+            obj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        } 
+	}
+}
