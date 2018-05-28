c560cba5e7df6c258d049ae1b18f62c3b36bac76
mametaro117 <ytfeek1@yahoo.co.jp>
2018-05-28 15:38:57 +0900

    バトルマネージャーの作成

---
+++ DoubutuDev/Assets/01.Scripts/Manager/BattleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+
+    private static GameObject[] AllAnimals;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void AddAnimals(GameObject obj)
+    {
+        //AllAnimals[] = obj;
+    }
+}
