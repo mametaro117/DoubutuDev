b295d37b9627343e376b84a69e163a910881c9b2
sunauru8nm <suna@outlook.jp>
2018-05-29 19:13:04 +0900

    Scriptableobjectの追加

---
+++ DoubutuDev/Assets/01.Scripts/Common/Totalstatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totalstatus : MonoBehaviour {
+
+    public int HitPoint;
+    public int Attack;
+    public int Size;
+    public bool IsSky;
+
+    public void SetStatus(int HP,int ATK, int size, bool issky)
+    {
+        HitPoint = HP;
+        Attack = ATK;
+        Size = size;
+        IsSky = issky;
+    }
+}
