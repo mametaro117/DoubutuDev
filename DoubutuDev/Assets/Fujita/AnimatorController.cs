89d7285abe5b9c5a71acc8dc51a7dc3a7ce7ec15
ryupanda <ryusuke0428tora@gmail.com>
2018-05-30 23:16:20 +0900

    Animator、Scriptの作成

---
+++ DoubutuDev/Assets/Fujita/AnimatorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour {
+
+    private Animator animator;
+		void Start ()
+    {
+       // animator = GetComponent<Animator>().setTrigger("Walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("", true);
+        }
+	}
+}
