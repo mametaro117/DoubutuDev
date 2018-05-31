8a9f7c14ab8f415f111b333ed5a942d0fab957c8
やたろう <karupisu.lobe.hanayo@gmail.com>
2018-05-31 16:26:28 +0900

    unityが起動しなかったので、起動後の確認

--- DoubutuDev/Assets/Scripts/ToolTips/ToolTipsView.cs
+++ DoubutuDev/Assets/Scripts/ToolTips/ToolTipsView.cs
@@ -7,24 +7,22 @@ public class ToolTipsView : MonoBehaviour
 {
     [HideInInspector]
     public GameObject obj;
+    public int Speed;
+    public int Cost;
+    public string Tokusei;
+    public RaycastHit2D hit;
 
-    private int Speed;
-    private int Cost;
-    private string Tokusei;
-    private RaycastHit2D hit;
 
     private void Start()
     {
         Speed = GameObject.Find("Animal_Data").GetComponent<ToolTips>().speed;
         Cost = GameObject.Find("Animal_Data").GetComponent<ToolTips>().cost;
         Tokusei = GameObject.Find("Animal_Data").GetComponent<ToolTips>().tokusei;
-        //Debug.Log(Speed);
     }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             hit = Physics2D.Raycast(pos, new Vector3(0, 0, 1), 100);
             Debug.Log("動物:" + hit.collider);
