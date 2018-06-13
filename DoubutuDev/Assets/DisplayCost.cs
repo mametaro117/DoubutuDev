1049e1b7e90415a7b6b9c2145a052908f22a1c00
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2018-06-13 18:58:20 +0900

    アイコンの下に消費コストの表示

---
+++ DoubutuDev/Assets/DisplayCost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCost : MonoBehaviour {
+
+	private GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		obj = transform.parent.gameObject;
+		Debug.Log (obj.transform.name);
+		ApplayCost ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ApplayCost(){
+		if (obj.GetComponent<WeaponButtonScript>() != null)
+		{
+			float costnum = obj.GetComponent<WeaponButtonScript>().GetCost();
+			transform.GetChild (0).GetComponent<Text> ().text = costnum.ToString ();
+		}
+		else if(obj.GetComponent<AnimalButtonScript>() != null)
+		{
+			float costnum = obj.GetComponent<AnimalButtonScript> ().GetCost ();
+			transform.GetChild (0).GetComponent<Text> ().text = costnum.ToString ();
+		}
+	}
+}
