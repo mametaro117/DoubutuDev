5e1c137cc6719db81119c9c25b6dd27f526a8730
ryupanda <ryusuke0428tora@gmail.com>
2018-06-21 23:46:48 +0900

    パラメーターをスクリプトで操作

---
+++ DoubutuDev/Assets/Fujita/AnimatorController3.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController3 : MonoBehaviour {
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetCompornent<Animator>();
+    }
+	
+    void TestEndAttack()
+    {
+        animator.SetBool("Attack, false");
+    }
+}
