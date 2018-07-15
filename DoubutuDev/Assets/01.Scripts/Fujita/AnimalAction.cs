1475dd809133f8fca1a07fb4ce6137c597a9a8aa
ryupanda <ryusuke0428tora@gmail.com>
2018-07-15 17:36:55 +0900

    像WalkAnimationの追加(上下の運動は無し)

---
+++ DoubutuDev/Assets/01.Scripts/Fujita/AnimalAction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAction : MonoBehaviour {
+
+    private bool _isUp = true;
+    private float _timer = 0;
+    private Vector3 _startPos;
+
+	// Use this for initialization
+	void Start () {
+        _startPos = GetComponent<RectTransform>().localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_isUp)
+        {
+            UpStep();
+        }
+        else
+        {
+            DownStep();
+        }
+	}
+
+    private void UpStep()
+    {
+        var pos = _startPos;
+        _timer += Time.deltaTime;
+        if (_timer >= 0.5f)
+        {
+            _isUp = false;
+            _timer -= 0.5f;
+            pos.y += (20f - _timer * 20f);
+        }
+        else
+        {
+            pos.y += Mathf.Lerp(0, 20f, _timer * 2);
+        }
+        GetComponent<RectTransform>().localPosition = pos;
+    }
+
+    private void DownStep()
+    {
+        var pos = _startPos;
+        _timer += Time.deltaTime;
+        if (_timer >= 0.5f)
+        {
+            _isUp = true;
+            _timer -= 0.5f;
+            pos.y += _timer * 20f;
+        }
+        else
+        {
+            pos.y += Mathf.Lerp(0, 20f, _timer * 2);
+        }
+        GetComponent<RectTransform>().localPosition = pos;
+    }
+}
