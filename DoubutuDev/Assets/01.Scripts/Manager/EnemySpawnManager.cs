0ffd57a1a324674527f6cc6ae527385a8ea5b462
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2018-07-17 09:39:20 +0900

    EnemySpawnManager仮実装 [1] 自分のキャラの数で判定

--- DoubutuDev/Assets/01.Scripts/Manager/EnemySpawnManager.cs
+++ DoubutuDev/Assets/01.Scripts/Manager/EnemySpawnManager.cs
@@ -40,8 +40,45 @@ public class EnemySpawnManager : MonoBehaviour {
 
     public void EnemyInstance()
     {
-        int rand = Random.Range(0, 5);
-        Instantiate(EnemyPrefab, new Vector3(SpawnPositions[rand].position.x, SpawnPositions[rand].position.y, SpawnPositions[rand].position.z), SpawnPositions[rand].rotation);
+        PowerCheck();
+        int SpawnPos = SetSpawnPos();
+        if(SpawnPos == -1)
+        {
+            SpawnPos = Random.Range(0, 5);
+        }
+        Instantiate(EnemyPrefab, new Vector3(SpawnPositions[SpawnPos].position.x, SpawnPositions[SpawnPos].position.y, SpawnPositions[SpawnPos].position.z), SpawnPositions[SpawnPos].rotation);
+    }
+
+    private int SetSpawnPos()
+    {
+        List<int> RaneNum = new List<int>();
+        int MaxPow = 0;
+        for(int i = 0; i < LanePowers.Length; i++)
+        {
+            //パワーを比較
+            //最大値より大きければリストを初期化してからレーン番号を入れる
+            if(LanePowers[i] > MaxPow)
+            {
+                MaxPow = LanePowers[i];
+                RaneNum = new List<int>();
+                RaneNum.Add(i);
+            }
+            //最大値と一緒ならレーン番号を追加する
+            else if(LanePowers[i] == MaxPow)
+            {
+                RaneNum.Add(i);
+            }
+        }
+        //*/
+        string str = "";
+        for(int i = 0; i < RaneNum.Count; i++)
+        {
+            str = str + i + "_";
+        }
+        Debug.Log(str);
+        //*/
+        //複数ある場合はランダムで返す
+        return RaneNum[Random.Range(0, RaneNum.Count)];
     }
 
     RaycastHit2D[] hit;
@@ -54,13 +91,14 @@ public class EnemySpawnManager : MonoBehaviour {
         for(int i = 0; i < 5; i++)
         {
             LanePowers[i] = 0;  //  初期化
-            hit = Physics2D.BoxCastAll(SpawnPositions[i].position, new Vector2(1, 2), 0f, Vector2.right, 10f);
+            hit = Physics2D.BoxCastAll(SpawnPositions[i].position, new Vector2(1, 1.6875f), 0f, Vector2.right, 10f);
             Debug.Log("BoxCast:" + hit.Length);
             for (int k = 0; hit.Length > k; k++)
             {
                 Debug.Log(k + "番目:"  + hit[k].collider.name);
                 if (hit[k].collider.tag == "Animal")
                     LanePowers[i]++;
+                Debug.Log(SpawnPositions[i].position);
             }
         }
     }
