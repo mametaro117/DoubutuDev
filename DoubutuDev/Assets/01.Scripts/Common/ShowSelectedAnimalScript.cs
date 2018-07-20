856299fc929c29987c8de239c1e2a5582262c5f1
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2018-07-20 10:38:10 +0900

    武器選択の空き場所に動物を出す所まで

--- DoubutuDev/Assets/01.Scripts/Common/ShowSelectedAnimalScript.cs
+++ DoubutuDev/Assets/01.Scripts/Common/ShowSelectedAnimalScript.cs
@@ -10,6 +10,70 @@ public class ShowSelectedAnimalScript : MonoBehaviour {
     private GameObject Hukurou;
     [SerializeField]
     private GameObject Zou;
+    [SerializeField]
+    private GameObject SelectedAnimal;
+    [SerializeField]
+    private int AnimalNum;
+    private Animalstate S_Animalstate;
+    private Totalstatus S_Totalstatus;
+    private AnimalScript S_Animalscript;
+    [SerializeField]
+    private GameObject Rabbit_Anim;
+    [SerializeField]
+    private CharacterController Zou_Anim;
+    private CharacterController _Animation;
 
+    public void ShowAnimal(GameObject Box)
+    {
+        int BoxNum = int.Parse(Box.name.Substring(Box.name.Length - 1));
+        WindowChangeScript _Param = GetComponent<WindowChangeScript>();
+        AnimalNum = _Param.AnimalAndWeaponList[BoxNum - 1, 0];
+        switch(AnimalNum)
+        {
+            case 0:
+                SelectedAnimal = Instantiate(Rabbit) as GameObject;
+                //_Animation = 
+                break;
+            case 1:
+                SelectedAnimal = Instantiate(Hukurou) as GameObject;
+                break;
+            case 2:
+                SelectedAnimal = Instantiate(Zou) as GameObject;
+                break;
+            default:
+                Debug.Log("未実装アニマルを選んだね？");
+                break;
+        }
+        S_Animalstate = SelectedAnimal.GetComponent<Animalstate>();
+        if(S_Animalstate != null)
+        {
+            S_Animalstate.enabled = !S_Animalstate.enabled;
+        }
+        S_Totalstatus = SelectedAnimal.GetComponent<Totalstatus>();
+        if(S_Totalstatus != null)
+        {
+            S_Totalstatus.enabled = !S_Totalstatus.enabled;
+        }
+        S_Animalscript = SelectedAnimal.GetComponent<AnimalScript>();
+        if(S_Animalscript != null)
+        {
+            S_Animalscript.enabled = !S_Animalscript.enabled;
+        }
+        SelectedAnimal.transform.position = new Vector3(5, -1, 0);
+        SelectedAnimal.transform.localScale = new Vector3(1.5f , 1.5f , 1.5f);
+        if(SelectedAnimal.transform.Find("HPBer").gameObject != null)
+        {
+            Destroy(SelectedAnimal.transform.Find("HPBer").gameObject);
+        }
+        if (SelectedAnimal.transform.Find("SkillBer").gameObject != null)
+        {
+            Destroy(SelectedAnimal.transform.Find("SkillBer").gameObject);
+        }
+    }
 
+    public void DestroyAnimal()
+    {
+        Destroy(SelectedAnimal);
+        SelectedAnimal = null;
+    }
 }
