786c04ce0a8564ae1d8e186915307ebf89641e26
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2018-08-24 15:09:54 +0900

    背景とタワー画像の追加

--- DoubutuDev/Assets/01.Scripts/Manager/DebugScript.cs
+++ DoubutuDev/Assets/01.Scripts/Manager/DebugScript.cs
@@ -2,19 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DebugScript : MonoBehaviour {
 
-    [SerializeField] private float PenguinHP;
-    [SerializeField] private float PenguinATK;
-    [SerializeField] private float PenguinSPD;
+    public bool Debug = false;
+    
+    public UnitTable unitTable;
 
-    [SerializeField] private float RabbitHP;
-    [SerializeField] private float RabbitATK;
-    [SerializeField] private float RabbitSPD;
-    [SerializeField] private float ElephantHP;
-    [SerializeField] private float ElephantATK;
-    [SerializeField] private float ElephantSPD;
-    //    [SerializeField] private float PenguinHP;
+    
 
     // Use this for initialization
     void Start () {
