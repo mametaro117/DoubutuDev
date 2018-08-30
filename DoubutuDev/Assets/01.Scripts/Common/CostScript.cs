ec3a6156ffe91d1f3c299c00eccafe0145a0d0bb
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2018-08-30 16:10:13 +0900

    ・動物のコストバーにコルーチンを実装(整数値が変化した時に、バーの濃い部分が時間を掛けて動く)の、ver1

--- DoubutuDev/Assets/01.Scripts/Common/CostScript.cs
+++ DoubutuDev/Assets/01.Scripts/Common/CostScript.cs
@@ -10,8 +10,8 @@ public class CostScript : MonoBehaviour {
     private const float MaxWeaponCost = 900;
 
     //  コスト初期値
-    private float DefaultAnimalCost = 300;
-    private float DefaultWeaponCost = 300;
+    private float DefaultAnimalCost = 100;
+    private float DefaultWeaponCost = 100;
 
     //  1秒あたりのコスト上昇値
     private float AddAnimalCost = 50;
@@ -28,21 +28,45 @@ public class CostScript : MonoBehaviour {
     private float WeaponCostPoint;
     //  幅を変更するための参照
     [SerializeField]
-    private GameObject AnimalrectObj, WeaponrectObj;
-    private RectTransform AnimalRect, WeaponRect;
+    private GameObject MainAnimalrectObj, MainWeaponrectObj;
+    [SerializeField]
+    private GameObject SubAnimalrectObj, SubWeaponrectObj;
+    private RectTransform MainAnimalRect, MainWeaponRect;
+    private RectTransform SubAnimalRect, SubWeaponRect;
     //  選択状態のオブジェクト参照用
     private GameObject AnimalObj = null, WeaponObj = null;
-
+    //コルーチン発動用
+    private int Retention_AnimalCost;
+    private int Retention_WeaponCost;
+    private int Time_AnimalCost;
+    private int Time_WeaponCost;
+    private int Target_AnimalCost;
+    private int Target_WeaponCost;
+    private float Diff_AnimalCost;
+    private float Diff_WeaponCost;
+    float Cor_CostPoint_AnimalCost;
+    float Cor_CostPoint_WeaponCost;
+    private int CorTime = 20;
+    private bool isCorRunning_AnimalCost = false;
+    private bool isCorRunning_WeaponCost = false;
 
     void Start () {
         //  バーのサイズ設定
-        BarRect = AnimalrectObj.GetComponent<RectTransform>().sizeDelta;
+        BarRect = MainAnimalrectObj.GetComponent<RectTransform>().sizeDelta;
         //  コストの初期化
         AnimalCostPoint = DefaultAnimalCost;
         WeaponCostPoint = DefaultWeaponCost;
         //  warning回避用にGameObjectからGetComponent
-        AnimalRect = AnimalrectObj.GetComponent<RectTransform>();
-        WeaponRect = WeaponrectObj.GetComponent<RectTransform>();
+        MainAnimalRect = MainAnimalrectObj.GetComponent<RectTransform>();
+        MainWeaponRect = MainWeaponrectObj.GetComponent<RectTransform>();
+        SubAnimalRect = SubAnimalrectObj.GetComponent<RectTransform>();
+        SubWeaponRect = SubWeaponrectObj.GetComponent<RectTransform>();
+        // コルーチン誤動作防止
+        Retention_AnimalCost = (int)Mathf.Floor(AnimalCostPoint / 100);
+        Retention_WeaponCost = (int)Mathf.Floor(WeaponCostPoint / 100);
+        //コストバー初期化
+        MainAnimalRect.sizeDelta = new Vector2(BarRect.x * (AnimalCostPoint / MaxAnimalCost), MainAnimalRect.sizeDelta.y);
+        MainWeaponRect.sizeDelta = new Vector2(BarRect.x * (WeaponCostPoint / MaxAnimalCost), MainWeaponRect.sizeDelta.y);
     }
 	
 	void Update () {
@@ -120,9 +144,57 @@ public class CostScript : MonoBehaviour {
     // 値をコストバーに反映
     void CostFixed()
     {
-        AnimalCostTxt.GetComponent<Text>().text = Mathf.Floor(AnimalCostPoint / 100).ToString("F0");
-        WeaponCostText.GetComponent<Text>().text = Mathf.Floor(WeaponCostPoint / 100).ToString("F0");
-        AnimalRect.sizeDelta = new Vector2(BarRect.x * (AnimalCostPoint / MaxAnimalCost), AnimalRect.sizeDelta.y);
-        WeaponRect.sizeDelta = new Vector2(BarRect.x * (WeaponCostPoint / MaxWeaponCost), WeaponRect.sizeDelta.y);
+        int AnimalCostInt = (int)Mathf.Floor(AnimalCostPoint / 100);
+        int WeaponCostInt = (int)Mathf.Floor(WeaponCostPoint / 100);
+
+        AnimalCostTxt.GetComponent<Text>().text = AnimalCostInt.ToString();
+        WeaponCostText.GetComponent<Text>().text = WeaponCostInt.ToString();
+
+        if(AnimalCostInt != Retention_AnimalCost)
+        {
+            if(isCorRunning_AnimalCost)
+            {
+                Debug.Log("AnimalCostコルーチンの内容を変更しました");
+                Time_AnimalCost = CorTime;
+                Diff_AnimalCost = (AnimalCostInt - Cor_CostPoint_AnimalCost) / Time_AnimalCost;
+                Debug.Log(Diff_AnimalCost);
+            }
+            else
+            {
+                StartCoroutine(Cor_AnimalCost(AnimalCostInt - Retention_AnimalCost, Retention_AnimalCost));
+            }
+            Retention_AnimalCost = AnimalCostInt;
+        }
+
+        MainWeaponRect.sizeDelta = new Vector2(BarRect.x * (WeaponCostInt * 100 / MaxAnimalCost), MainWeaponRect.sizeDelta.y);
+
+        SubAnimalRect.sizeDelta = new Vector2(BarRect.x * (AnimalCostPoint / MaxAnimalCost), SubAnimalRect.sizeDelta.y);
+        SubWeaponRect.sizeDelta = new Vector2(BarRect.x * (WeaponCostPoint / MaxWeaponCost), SubWeaponRect.sizeDelta.y);
     }
+
+    //Mainのコストバーを変化させるコルーチン
+    //for動物
+    IEnumerator Cor_AnimalCost(int Change, int OriginAnimalCost)
+    {
+        Debug.Log("StartCol_Animal");
+        isCorRunning_AnimalCost = true;
+        Time_AnimalCost = CorTime;
+        Diff_AnimalCost = (float)Change / Time_AnimalCost;
+        Cor_CostPoint_AnimalCost = OriginAnimalCost;
+
+        Debug.Log("------------------");
+        //指定の所までもっていく
+        while(Time_AnimalCost > 0)
+        {
+            Time_AnimalCost--;
+            Cor_CostPoint_AnimalCost += Diff_AnimalCost;
+            //Debug.Log(Cor_CostPoint_AnimalCost);
+            MainAnimalRect.sizeDelta = new Vector2(BarRect.x * (Cor_CostPoint_AnimalCost * 100 / MaxAnimalCost), MainAnimalRect.sizeDelta.y);
+            yield return null;
+        }
+        isCorRunning_AnimalCost = false;
+        Debug.Log("EndCol_Animal");
+        yield break;
+    }
+    //for武器
 }
