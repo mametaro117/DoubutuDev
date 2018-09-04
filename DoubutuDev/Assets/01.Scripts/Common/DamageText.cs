ed95e8459fb7b925c82eb8fc241269f2a651c0d6
やたろう <karupisu.lobe.hanayo@gmail.com>
2018-09-04 11:24:02 +0900

    DamageTextのMeshRendererとTextMeshを外して、SpriteRendererに変えた

--- DoubutuDev/Assets/01.Scripts/Common/DamageText.cs
+++ DoubutuDev/Assets/01.Scripts/Common/DamageText.cs
@@ -76,10 +76,10 @@ public class DamageText : MonoBehaviour {
     /// <param name="damage">ダメージの値</param>
     /// <param name="damageTextColor">ダメージカラー(デフォルトは黒)</param>
     
-
-    List<int> listNum = new List<int>();
+        
     [SerializeField]
-    private Sprite[] numImage;
+    private Sprite[] sprite = new Sprite[10];
+    SpriteRenderer sr;
 
     public void DiplayText_Animal(Vector3 pos, float damage, DamageTextColor damageTextColor = DamageTextColor.Defalut)
     {
@@ -90,45 +90,24 @@ public class DamageText : MonoBehaviour {
         obj.GetComponent<TextMesh>().text = damage.ToString();
         obj.GetComponent<TextMesh>().color = _damageTextColors[damageTextColor];
         //Debug.Log(BattleManager.Instance.TypeCheckNum);
-
-
-        GameObject.Find("DamageTextImage").GetComponent<Image>().sprite = numImage[listNum[0]];
-        for (int i = 1; i < listNum.Count; i++)
-        {
-            RectTransform _damageTextImage = (RectTransform)Instantiate(GameObject.Find("DamageTextImage")).transform;
-            _damageTextImage.SetParent(this.transform, false);
-            _damageTextImage.localPosition = new Vector2(_damageTextImage.localPosition.x - _damageTextImage.sizeDelta.x * i, _damageTextImage.localPosition.y);
-            _damageTextImage.GetComponent<Image>().sprite = numImage[listNum[i]];
-        }
-
+        
         switch (BattleManager.Instance.TypeCheckNum)
         {
             case 0:
-                Debug.Log("Player同じ武器の攻撃");
+                sr.GetComponent<SpriteRenderer>().sprite = sprite[BattleManager.Instance.TypeCheckNum];
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(-60, -100), Random.Range(100, 180)));
                 break;
             case 1:
-                Debug.Log("Player自分が有利");
+
+                obj.GetComponent<TextMesh>().fontSize = 50;
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(-60, -100), Random.Range(100, 180)));
                 break;
             case 2:
-                Debug.Log("Player相手が有利");
+
+                obj.GetComponent<TextMesh>().fontSize = 30;
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(-60, -100), Random.Range(100, 180)));
                 break;
         }
-
-      /*  switch (BattleManager.Instance.TypeCheckNum)
-        {   
-            case 0:
-                obj.GetComponent<TextMesh>().fontSize = 25;
-                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(20, 80), Random.Range(100, 180)));
-                break;
-            case 1:
-                obj.GetComponent<TextMesh>().fontSize = 50;
-                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(20, 80), Random.Range(100, 180)));
-                break;
-            case 2:
-                obj.GetComponent<TextMesh>().fontSize = 30;
-                obj.GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(20, 80), Random.Range(100, 180)));
-                break;
-        }*/
         StartCoroutine(DestryText(obj));
     }
     public void DiplayText_Enemy(Vector3 pos, float damage, DamageTextColor damageTextColor = DamageTextColor.Defalut)
