28ef4d27e4ba51493631dcd8d0f191cee0a413d0
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2018-09-07 11:27:08 +0900

    コスト不足などのUI追加、動物の敵Ver画像追加

--- DoubutuDev/Assets/01.Scripts/Manager/SpawnManager.cs
+++ DoubutuDev/Assets/01.Scripts/Manager/SpawnManager.cs
@@ -3,10 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SpawnManager : MonoBehaviour {
 
     private CostScript costScript;
+
+    [SerializeField]
+    private GameObject CostErrorImage;
+    [SerializeField]
+    private GameObject ChoiceErrorImage;
 
     [SerializeField]
     private GameObject[] units = new GameObject[8];
@@ -40,14 +46,14 @@ public class SpawnManager : MonoBehaviour {
             else
             {
                 //  コストが足りない場合画面に表示
-
+                StartCoroutine(FadeCostErrorImage());
                 Debug.Log("コストが足りない");
             }
         }
         else
         {
             //  ボタンを選んでないよを表示
-
+            StartCoroutine(FadeChoiceErrorImage());
             Debug.Log("ボタン選んでないよ");
         }
             
@@ -62,5 +68,48 @@ public class SpawnManager : MonoBehaviour {
     public void SetAnimalsPrefab(GameObject[] _animals)
     {
         units = _animals;
+    }
+
+    private bool isFadeCostError = false;
+    private bool isFadeChoiceError = false;
+
+    IEnumerator FadeCostErrorImage()
+    {
+        if (!isFadeCostError)
+        {
+            isFadeCostError = true;
+            CostErrorImage.SetActive(true);
+            CostErrorImage.GetComponent<CanvasGroup>().alpha = 1;
+            yield return new WaitForSeconds(0.2f);
+            for (int i = 0; i <= 20; i++)
+            {
+                CostErrorImage.GetComponent<CanvasGroup>().alpha = 1 - (0.04f * i);
+                yield return new WaitForSeconds(0.04f);
+            }
+            CostErrorImage.GetComponent<CanvasGroup>().alpha = 0;
+            CostErrorImage.SetActive(false);
+            isFadeCostError = false;
+        }
+        yield break;
+    }
+
+    IEnumerator FadeChoiceErrorImage()
+    {
+        if (!isFadeChoiceError)
+        {
+            isFadeChoiceError = true;
+            ChoiceErrorImage.SetActive(true);
+            ChoiceErrorImage.GetComponent<CanvasGroup>().alpha = 1;
+            yield return new WaitForSeconds(0.2f);
+            for (int i = 0; i <= 6; i++)
+            {
+                ChoiceErrorImage.GetComponent<CanvasGroup>().alpha = 1 - (0.167f * i);
+                yield return new WaitForSeconds(0.05f);
+            }
+            ChoiceErrorImage.GetComponent<CanvasGroup>().alpha = 0;
+            ChoiceErrorImage.SetActive(false);
+            isFadeChoiceError = false;
+        }
+        yield break;
     }
 }
